5b459d0d476172ea18a5505e861facc6e40e7809
Vladimir <wl.sulimov@gmail.com>
2022-07-13 01:09:37 +0300

    feat: hamburger menu v2

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const hamburgerIco = document.querySelector(".hamburger-ico");
+const hamburger = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
+const account = document.querySelector(".account");
+const cross = document.querySelector(".menu-cross");
 
-hamburgerIco.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
     navigation.classList.toggle("active");
 })
+cross.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+})
+document.querySelectorAll(".navigation__link").forEach(n => n.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+}))
