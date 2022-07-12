aaa326bc467514f671e9478e68ee71ef029a81d7
Vladimir <wl.sulimov@gmail.com>
2022-07-13 01:50:56 +0300

    fix: add minor hamburger menu fixes

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ hamburger.addEventListener("click", () => {
     navigation.classList.toggle("active");
 })
 cross.addEventListener("click", () => {
-  navigation.classList.toggle("active");
+  navigation.classList.remove("active");
 })
 document.querySelectorAll(".navigation__link").forEach(n => n.addEventListener("click", () => {
-  navigation.classList.toggle("active");
+  navigation.classList.remove("active");
 }))
+
+// window.addEventListener("click", function(e){   
+//   if (document.querySelector(".navigation").contains(e.target)){
+//     // Clicked in box
+//   } else{
+//     navigation.classList.remove("active");
+//   }
+// });
